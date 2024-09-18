47e177617972b72ff54f12a1e532e242a0707045
byiringiroaimefils <aimefils173@gmail.com>
2024-09-18 21:53:19 +0200

    New version

--- DataExtraction.py
+++ DataExtraction.py
@@ -2,25 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 
-url = "https://igihe.com/index.php"
+url = "https://www.newtimes.co.rw"
 response = requests.get(url)
 
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
-paragraphs = soup.find_all('p')
+title_articles= soup.find_all("h1", class_="title article")
+article_bodies= soup.find_all("div", class_="article-body")
 
 workbook = openpyxl.Workbook()
 sheet = workbook.active
 
 
-sheet['A1'] = 'Paragraph Number'
-sheet['B1'] = 'Text'
+sheet['A1'] = 'Title Article'
+sheet['B1'] = 'article Body'
 
-for i, p in enumerate(paragraphs):
-    sheet.cell(row=i+1, column=1, value=i)
-    sheet.cell(row=i+1, column=2, value=p.text)
+for i, p in  enumerate(zip(title_articles, article_bodies), start=2) :
+        sheet.cell(row=i+1, column=1, value=p.text.strip())
+        sheet.cell(row=i+1, column=2, value=p.text.strip())
 
-workbook.save('igihe_data.xlsx')
+workbook.save('TheNew_data.xlsx')
 print("Data has been extracted ")
 
